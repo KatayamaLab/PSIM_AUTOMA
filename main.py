5beefc2d2c92b0cb92455e7ba62b03416609e4c0
yamanaka-ken <7320144@ed.tus.ac.jp>
2023-06-13 17:16:02 +0900

    combine main.py with my code

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import subprocess
+import nyquist
+import numpy as np
+import matplotlib.pyplot as plt
 
+path="./Impedance.txt"
 freq = 0.1
+
 for i in range(11):
-    subprocess.call(f'C:\Powersim\PSIM12.0.1_Softkey_X64\PsimCmd.exe -i "C:/Users/ymnk2/Downloads/EVbattery/libdiag007capacitor.psimsch" -o "./libdiag007capacitor-{i}.txt" -v "fsig={freq}"'.split(' '))
+    subprocess.call(f'C:\Powersim\PSIM12.0.1_Softkey_X64\PsimCmd.exe -i "C:/Users/ymnk2/Downloads/EVbattery/libdiag007capacitor.psimsch" -o "./libdiag007capacitor_{i}.txt" -v "fsig={freq}"'.split(' '))
+    nyquist.make_graph(f"./libdiag007capacitor_{i}.txt", freq)
     freq = freq*10**(1/5)
 
+nyquist.f.close()
+Im = np.genfromtxt(path,usecols=(1))
+Re = np.genfromtxt(path,usecols=(0))
+fig,ax2 = plt.subplots()
+#ax2.plot(Re,Im)
+ax2.invert_yaxis()
+plt.scatter(Re,Im)
---
+++ nyquist.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+path="./Impedance.txt"
+f=open(path,"w")
+
+
+def make_graph(file_path,frequency):
+    sum_v=0
+    sum_i=0
+    # グラフの作成
+    yI = np.genfromtxt(file_path,usecols=(2))
+    yV = np.genfromtxt(file_path,usecols=(3))
+    x = np.genfromtxt(file_path,usecols=(0))
+
+
+    for i in range(1,len(x)):
+        sum_v+=yV[i]
+        sum_i+=yI[i]
+    ave_v=sum_v/(len(x))
+    ave_i=sum_i/(len(x))
+    liv=yV[1:]-ave_v
+    liI=yI[1:]-ave_i
+
+    #fig,ax1 = plt.subplots()
+
+    #ax1.plot(x,yV-ave_v,x,yI-ave_i)
+    #plt.show()
+
+    sum_v=0
+    sum_i=0
+    # グラフの作成
+    yI = np.genfromtxt(file_path,usecols=(2))
+    yV = np.genfromtxt(file_path,usecols=(3))
+    x = np.genfromtxt(file_path,usecols=(0))
+
+    for i in range(1,len(x)):
+        sum_v+=yV[i]
+        sum_i+=yI[i]
+    ave_v=sum_v/(len(x))
+    ave_i=sum_i/(len(x))
+    liv=yV[1:]-ave_v
+    liI=yI[1:]-ave_i
+    V=np.max(yV[1:]-ave_v)
+    I=np.max(yI[1:]-ave_i)
+    Z=V/I
+
+    li1=[]
+
+
+    for i in range(1,len(x)-2):
+        if liv[i]<0:
+            if liv[i+1]>=0:
+                li1.append((x[i]+x[i+1])/2)
+
+        if liI[i]<0:
+            if liI[i+1]>=0:
+                li1.append((x[i]+x[i+1])/2)
+
+
+    li = [r - l for l, r in zip(li1, li1[1:])]
+
+    ave_theta=np.min(np.abs(li))      #初期化
+    theta=np.abs(2*np.pi*frequency*ave_theta)
+    x_n=Z*np.cos(theta)
+    y_n=-1*Z*np.sin(theta)
+    if x_n>0:
+        f.write(str(x_n)+"  ")
+        f.write(str(y_n)+"\n")
+    print(x_n, y_n,"f=",frequency,Z,theta)
+
+
