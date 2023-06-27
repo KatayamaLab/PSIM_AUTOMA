8e304c89ede27596f4b3f4ddf722803c652768cf
yamanaka-ken <7320144@ed.tus.ac.jp>
2023-06-27 23:58:27 +0900

    improve

--- FFT.py
+++ FFT.py
@@ -4,79 +4,83 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-import math
+# import math
 import pandas as pd
 
-path = "./Impedance2.txt"
+
+path = "./Impedance.txt"
 
 with open(path, "w") as f:
     pass
 
-
 def ChangeImpedance(V_re, V_im, I_re, I_im):
     V=np.sqrt(V_re**2+V_im**2)
     I=np.sqrt(I_re**2+I_im**2)
-    print("(V,I)=",V,I)
+    
     Z = V/I
-    theta = math.atan(V_im/V_re)-math.atan(I_im/I_re)
-    if math.pi/2 < np.abs(theta):
-        theta = np.abs(theta) - math.pi
+    theta = -1*np.abs(np.arctan(V_im/V_re)-np.arctan(I_im/I_re))
+
+    if np.pi/2 < np.abs(theta):
+        theta = np.abs(theta) - np.pi
 
     print("(Z,theta)=",Z,theta)
-    Z_re=(Z*math.cos(theta))
-    Z_im=(Z*math.sin(theta))
+    Z_re=(Z*np.cos(theta))
+    Z_im=(Z*np.sin(theta))
 
     with open(path, "a") as f:
         f.write(str(Z_re)+"   ")
-        f.write(str(Z_im)+"\n")
-
-    
+        f.write(str(Z_im)+"\n")    
     print(Z_re,Z_im)
 
 
 def make_graph(path):
     # データの読み込み
-    cols0 = pd.read_table(path,usecols=[0],delimiter="  ",engine="python")
-    cols1 = pd.read_table(path,usecols=[1],delimiter="  ",engine="python")
-    cols2 = pd.read_table(path,usecols=[2],delimiter="  ",engine="python")
+    # cols0 = pd.read_table(path, usecols = [0], delimiter = "  ", engine = "python", dtype = "float", skiprows = 1)
+    # cols1 = pd.read_table(path, usecols = [2], delimiter = "  ", engine = "python", dtype = "float", skiprows = 1)
+    # cols2 = pd.read_table(path, usecols = [3], delimiter = "  ", engine = "python", dtype = "float", skiprows = 1)
+    df = pd.read_table(
+        path,
+        header=0,
+        delim_whitespace=True)
+    # print(df)
 
-    time = cols0.iloc[:,0]
-    value_I = cols1.iloc[:,0]
-    value_V =cols2.iloc[:,0]
+    time = np.array(df['Time'], dtype = float)
+    value_I = np.array(df['Iac1'], dtype = float)
+    value_V = np.array(df['Vac1'], dtype = float)
 
+    dt = time[2]-time[1]
     N = len(time)                    # サンプル数
-    dt = time[2]-time[1]     # サンプリング周期 [s] ※固定ステップか確認!!
-
-
-    FI = np.fft.fft( value_I, N) # Iac1
-    FV = np.fft.fft( value_V, N) # Vac1
+    
+    FI = np.fft.fft(value_I, N) # Iac1
+    FV = np.fft.fft(value_V, N) # Vac1
     freq = np.fft.fftfreq(N, d=dt) # 周波数
 
-    Amp_I = 2*np.abs(FI/(N/2)) # Iac1
-    Amp_V = 2*np.abs(FV/(N/2)) # Vac1
-    Amp_I_max=np.max(Amp_I[1:N//2])
-    Amp_V_max=np.max(Amp_V[1:N//2]) 
+    FI[0] = 0
+    FV[0] = 0
+    Amp_I = 2 * np.abs(FI/(N/2)) # Iac1
+    Amp_V = 2 * np.abs(FV/(N/2)) # Vac1
+    Amp_I_max = max(Amp_I[1:N//2])
+    Amp_V_max = max(Amp_V[1:N//2]) 
 
     # signal.find_peaks(振度, 高さ)の戻り値は極大値のindexと高さ
     # 第二引数以上の高さのindexを取り出す
     # Amp_c_maxで高さの目安を計算し、heightに代入
-    index_I, h_I = signal.find_peaks(Amp_I, height = Amp_I_max - Amp_I_max/100)
-    index_V, h_V = signal.find_peaks(Amp_V, height = Amp_V_max - Amp_V_max/100)
+    # Amp_I_max = max(Amp_I[1:N//2])
+    # Amp_V_max = max(Amp_V[1:N//2]) 
+    # index_I = np.where(Amp_I == Amp_I_max)
+    # index_V = np.where(Amp_V == Amp_V_max)
+    # plt.show()
+    # index_I, _I = signal.find_peaks(Amp_I,height = Amp_I_max*0.8)
+    # index_V, _I = signal.find_peaks(Amp_I,height = Amp_I_max*0.8)
 
- 
-    plt.show()
+    V_cartesian = FV[3] / (N/2)
+    I_cartesian = FI[3] / (N/2)
 
-    V_cartesian = FV[index_V[0]] / (N/2)
-    I_cartesian = FI[index_I[0]] / (N/2)
-
+    V_re,V_im,I_re,I_im = 0, 0, 0, 0
     V_re = V_cartesian.real
     V_im = V_cartesian.imag
     I_re = I_cartesian.real
-    I_im = I_cartesian.imag    
+    I_im = I_cartesian.imag
+    print("(V,I)=",Amp_V[3], Amp_I[3])
 
     ChangeImpedance(V_re, V_im, I_re, I_im)
-
-
-
-
-
--- main.py
+++ main.py
@@ -24,13 +24,13 @@ out = os.getcwd()
 # 理論値
 # idea_path="C:/Users/ymnk2/Desktop/python-practice/mkgraph/idea.txt"
 # 入力ファイル(psimsch)
-psimfile_in = "C:/Users/ymnk2/Documents/GitHub/PSIM_AUTOMA/libcell.psimsch"
+psimfile_in = "C:/Users/ymnk2/Documents/GitHub/PSIM_AUTOMA/libdiag007capacitor.psimsch"
 finit = 0.1
 freq = finit
 total_time = 1
 time_step = 10**(-5)
 
-with open("./Impedance2.txt", "w") as f:
+with open("./Impedance.txt", "w") as f:
     pass
 
 #  The format to call the command-line version of PSIM is as follows:
@@ -55,7 +55,7 @@ N = 21
 # PSIM起動および出力データ収集
 for i in range(N):
      total_time = 0.8 + 3 / freq
-     subprocess.call(f'C:/Powersim/PSIM12.0.1_Softkey_X64/PsimCmd.exe -i {psimfile_in} -o "{out}/libcell_{i}.txt" -v "fsig={freq}" -t "{total_time}" -s "{time_step}"'.split(' '))
+     subprocess.call(f'C:/Powersim/PSIM12.0.1_Softkey_X64/PsimCmd.exe -i {psimfile_in} -o "{out}/libdiag007capacitor_{i}.txt" -v "fsig={freq}" -t "{total_time}" -s "{time_step}"'.split(' '))
     #  imp.append(test.make_graph(f"C:/Users/ymnk2/Downloads/EVbattery/libdiag007capacitor_{i}.txt", freq, time_step))
     #  FFT.make_graph(f"C:/Users/ymnk2/Downloads/EVbattery/libdiag007capacitor_{i}.txt", freq, time_step)
      freq = freq*10**(1/5)
@@ -66,15 +66,15 @@ for i in range(N):
 freq = finit
 print(os.getcwd())
 os.chdir(f"{cd}/{A}")
-
 # データの読み込み
 for i in range(1,N):
-    FFT.make_graph(f"{out}/libcell_{i}.txt")
+    # FFT.make_graph(f"{out}/libdiag007capacitor_{i}.txt")
+    FFT.make_graph(f"c:/Users/ymnk2/Documents/GitHub/PSIM_AUTOMA/outdir/libdiag007capacitor_{i}.txt")
     print(100*i/20,"%" ,"has done!!","fsig=",freq)
     freq = freq*10**(1/5)
 
-Re = np.loadtxt(f"./Impedance2.txt", usecols=0)
-Im = np.loadtxt(f"./Impedance2.txt", usecols=1)
+Re = np.loadtxt("./Impedance.txt", usecols=0)
+Im = np.loadtxt("./Impedance.txt", usecols=1)
 
 # x = np.loadtxt(idea_path, usecols=1)
 # y = np.loadtxt(idea_path, usecols=2)
